742c1a70de05438eefefb73eca586dfc600f06e6
NeoEcc <niccolo3010.eccel@gmail.com>
2025-07-08 10:44:33 +0200

    Removed unused file

--- scripts/mitochondria_download.py
+++
@@ -1,9 +0,0 @@
-import quilt3 as q3
-
-if __name__ == "__main__":
-    
-    bucket_str = "s3://janelia-cosem-datasets"
-    folder_path = "/mnt/lustre-emmy-ssd/projects/nim00007/data/mitochondria/files/"
-
-    b = q3.Bucket("s3://janelia-cosem-datasets")
-    b.fetch("jrc_ctl-id8-2/jrc_ctl-id8-2.zarr/", "/mnt/lustre-emmy-ssd/projects/nim00007/data/mitochondria/files/example_dataset/jrc_ctl-id8-2.zarr/")
